93409c76c5a600a03df9779a5b1e2f34e7d1b11e
Allen Lucke <allenlucke@gmail.com>
2020-03-20 17:23:41 -0500

    basic python command line mad lib

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
-msg = "Hello World"
-print(msg)
+msg1 = "Hello World"
+firstName = "Allen"
+favorite_color = "plaid"
+msg2 = "is learning Python"
+msg1.capitalize
+print(msg1)
 
-msg.capitalize
 
+print(msg1, firstName, msg2, "his favorite color is", favorite_color)
+
+lastName = input("What is your last name?")
+
+print("Hello", firstName, lastName)
+
+quest = input("what is your quest")
+
+print("Well", firstName, lastName, "i bid you good luck on your quest to", quest)
