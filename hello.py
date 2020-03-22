41aa9b6fb86c0722e7ae09354de90012f2d46ea1
Allen Lucke <allenlucke@gmail.com>
2020-03-21 19:10:44 -0500

    more python fun on teamtreehouse

--- hello.py
+++ hello.py
@@ -6,12 +6,29 @@ msg1.capitalize
 print(msg1)
 
 
-print(msg1, firstName, msg2, "his favorite color is", favorite_color)
+# print(msg1, firstName, msg2, "his favorite color is", favorite_color)
 
-lastName = input("What is your last name?")
+# lastName = input("What is your last name?")
 
-print("Hello", firstName, lastName)
+# print("Hello", firstName, lastName)
 
-quest = input("what is your quest")
+# quest = input("what is your quest")
 
-print("Well", firstName, lastName, "i bid you good luck on your quest to", quest)
+# print("Well", firstName, lastName, "i bid you good luck on your quest to", quest)
+
+# PEMDAS = "please excuse my dear aunt sally"
+
+# print("Well", firstName, lastName, "\ni bid you good luck on your quest to", quest)
+
+print(favorite_color*20)
+
+#len stands for length
+print(len(favorite_color*20))
+
+print(favorite_color.upper())
+
+print(msg1.title())
+
+subject_template = "thanks for learning {} with us {}"
+
+print(subject_template.format("python", firstName))
